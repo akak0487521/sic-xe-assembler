9772735f1e1b90a3f150fef635160109f14e6425
alida <34068248+alida0224@users.noreply.github.com>
2018-11-22 21:14:19 +0800

    Add files via upload

--- sic_xe.c
+++ sic_xe.c
@@ -1,3 +1,25 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+
+    if (!argv[1]) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (!(fp = fopen(argv[1], "r"))) {
+        fprintf(stderr, "%s can't be opened.\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    
+    //while (!fgets(..., ..., ...)) {
+    //    ......
+    //}
+
+    fclose(fp);
+    
     return 0;
 }
