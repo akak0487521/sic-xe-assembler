9419b8d44c69a4cca4d17aac14538a9dfd486de5
alida <34068248+alida0224@users.noreply.github.com>
2018-11-24 02:27:50 +0800

    Add files via upload

--- sic_xe.c
+++ sic_xe.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define DELIMITER " \n\r\t"
 #define LINE_SIZE 256
+#define TOKEN_NUM 4
+
+char **split_line(char *);
 
 int main(int argc, char *argv[])
 {
     FILE *fp;
     char line[LINE_SIZE];
-
+    char **line_tokens;
+    
+    int LOCCTR = 0;
+    
     if (!argv[1]) {
         fprintf(stderr, "usage: %s filename\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    /******** Read file ********/
     if (!(fp = fopen(argv[1], "r"))) {
         fprintf(stderr, "%s can't be opened.\n", argv[1]);
         exit(EXIT_FAILURE);
@@ -20,10 +29,50 @@ int main(int argc, char *argv[])
 
     /******** Read line test ********/
     while (fgets(line, LINE_SIZE, fp)) {
-        printf("%s", line);
+        line_tokens = split_line(line);
+        
+        for (int i = 0; i < TOKEN_NUM; i++) {
+            if (line_tokens[i]) printf("%s\t\t", line_tokens[i]);
+        }
+        printf("\n");
+        
+        /****************************************************
+         *  read first input line                           *
+         *  if OPCODE == 'START' then                       *
+         *      begin                                       *
+         *          save #[OPERAND] as starting address     *
+         *          initialize LOCCTR to starting address   *
+         *          write line to intermediate file         *
+         *          read next input line                    *
+         *      end {if START}                              *
+         *  else                                            *
+         *      initialize LOCCTR to 0                      *
+        *****************************************************/
     }
-
+    
+    free(line_tokens);
+    
     fclose(fp);
     
     return 0;
+}
+
+char **split_line(char *line)
+{
+    char **line_tokens = calloc(5, sizeof(char *));
+    char *token;
+    
+    if (!line_tokens) {
+        fprintf(stderr, "Memory allocation error!");
+        exit(EXIT_FAILURE);
+    }
+    
+    token = strtok(line, DELIMITER);
+    for (int i = 0; i < TOKEN_NUM; i++) {
+        if (token == NULL) break;
+        line_tokens[i] = token;
+        token = strtok(NULL, DELIMITER);
+    }
+    
+    return line_tokens;
 }
